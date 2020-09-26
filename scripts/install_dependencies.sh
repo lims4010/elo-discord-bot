48aded25c7590c4d8b7781eb1bfe6d1b936251b6
lims4010 <lims4010@gmail.com>
2020-09-26 14:38:13 -0400

    deployment files

---
+++ scripts/install_dependencies.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+### Install dependencies
+# sudo apt update
+# sudo apt install python3-pip
+# sudo curl https://deb.nodesource.com/setup_10.x -o nodesource_setup.sh
+# sudo bash nodesource_setup.sh
+# sudo apt install nodejs -y
+# sudo npm install pm2@latest -g
+# cd /var/app/
+# pip3 install -r requirements.txt
+
+
+
---
+++ scripts/start_script.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+## Kill all pm2 processes
+cd /var/app/
+sudo pkill -f pm2
+
