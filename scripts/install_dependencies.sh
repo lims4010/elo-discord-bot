0f5c7e7add6bab2fb38fbbfef71a32396c7dd185
lims4010 <lims4010@gmail.com>
2020-09-26 20:54:53 -0400

    deployment debug

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -6,6 +6,7 @@
 # sudo curl https://deb.nodesource.com/setup_10.x -o nodesource_setup.sh
 # sudo bash nodesource_setup.sh
 # sudo apt install nodejs -y
+# sudo apt-get install awscli -y
 # sudo npm install pm2@latest -g
 # cd /var/app/
 # pip3 install -r requirements.txt
--- scripts/start_script.sh
+++ scripts/start_script.sh
@@ -4,3 +4,14 @@
 cd /var/app/
 sudo pkill -f pm2
 
+## Star pm2 service
+
+export MONGODB_SECRET=$(aws ssm get-parameters --region us-east-1 --names /elo-discord-bot/MONGODB_SECRET --query Parameters[0].Value) 
+export MONGODB_SECRET=$(echo "$MONGODB_SECRET"|tr -d '"')  
+
+export DISCORD_TOKEN=$(aws ssm get-parameters --region us-east-1 --names /elo-discord-bot/DISCORD_TOKEN --query Parameters[0].Value) 
+export DISCORD_TOKEN=$(echo "$DISCORD_TOKEN"|tr -d '"')  
+
+MONGODB_SECRET=$MONGODB_SECRET \
+DISCORD_TOKEN=$DISCORD_TOKEN \
+pm2 start botApp.py --interpreter=python3
