6d9a17b088846807e23108ed41f523ea84005334
lims4010 <lims4010@gmail.com>
2020-09-26 21:25:03 -0400

    deployment debug

--- scripts/start_script.sh
+++ scripts/start_script.sh
@@ -6,8 +6,6 @@ sudo pkill -f pm2
 
 ## Star pm2 service
 
-sudo touch test.txt
-
 export MONGODB_SECRET=$(aws ssm get-parameters --region us-east-1 --names /elo-discord-bot/MONGODB_SECRET --query Parameters[0].Value) 
 export MONGODB_SECRET=$(echo "$MONGODB_SECRET"|tr -d '"')  
 
@@ -16,4 +14,4 @@ export DISCORD_TOKEN=$(echo "$DISCORD_TOKEN"|tr -d '"')
 
 MONGODB_SECRET=$MONGODB_SECRET \
 DISCORD_TOKEN=$DISCORD_TOKEN \
-pm2 start botApp.py --interpreter=python3
+pm2 start botApp.py --interpreter=python3 > log.txt
